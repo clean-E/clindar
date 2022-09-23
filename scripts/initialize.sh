1b8e634a7cb3067bf499b70be41cba763cc60205
LeeJooYong <daramzycoder@gmail.com>
2022-09-23 10:01:16 +0900

    fix : 배포 자동화 시도해보는중

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/clindar/server
-sudo npm install -f
-sudo npm install pm2@latest -g
+npm install -f
+npm install pm2@latest -g
 sudo apt-get update
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
