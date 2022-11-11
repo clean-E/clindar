2da4969fda75a0bc53b415106fd2b3f1795b2c74
LeeJooYong <daramzycoder@gmail.com>
2022-11-11 21:56:31 +0900

    feat: 유저쿼리 추가중

--- server/src/models/user/user.providers.ts
+++ server/src/models/user/user.providers.ts
@@ -1,4 +1,6 @@
 import { Connection } from 'mongoose';
+import { RecordSchema } from 'src/schemas/record.schema';
+import { ScheduleSchema } from 'src/schemas/schedule.schema';
 import { UserSchema } from 'src/schemas/user.schema';
 
 export const UserProviders = [
@@ -6,6 +8,18 @@ export const UserProviders = [
     provide: 'USER_MODEL',
     useFactory: (connection: Connection) =>
       connection.model('User', UserSchema, 'User'),
+    inject: ['DATABASE_CONNECTION'],
+  },
+  {
+    provide: 'RECORD_MODEL',
+    useFactory: (connection: Connection) =>
+      connection.model('Record', RecordSchema, 'Record'),
+    inject: ['DATABASE_CONNECTION'],
+  },
+  {
+    provide: 'SCHEDULE_MODEL',
+    useFactory: (connection: Connection) =>
+      connection.model('Schedule', ScheduleSchema, 'Schedule'),
     inject: ['DATABASE_CONNECTION'],
   },
 ];
--- server/src/models/user/user_query.service.ts
+++ server/src/models/user/user_query.service.ts
@@ -1,6 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ApolloError } from 'apollo-server-express';
 import { Model } from 'mongoose';
+import { Records } from 'src/schemas/record.schema';
+import { Schedule } from 'src/schemas/schedule.schema';
 import { ReturnUser, User } from 'src/schemas/user.schema';
 
 @Injectable()
@@ -8,12 +10,24 @@ export class UserQuery {
   constructor(
     @Inject('USER_MODEL')
     private userModel: Model<User>,
+    @Inject('RECORD_MODEL')
+    private recordModel: Model<Records>,
+    @Inject('SCHEDULE_MODEL')
+    private scheduleModel: Model<Schedule>,
   ) {}
 
   // 내 기록을 모아서 볼 수 있도록 - 암장별 기록
   // async getMyPage(email: string): Promise<ReturnUser> {
   //   const { nickname, myRecord } = await this.userModel.findOne({ email });
 
+  //   const records = {};
+  //   await Promise.all(
+  //     myRecord.map(async (id) => {
+  //       const recordInfo = await this.recordModel.findById(id);
+  //       const scheduleInfo = await this.scheduleModel.findById(recordInfo.sId);
+  //       const spotName = scheduleInfo.spot;
+  //     }),
+  //   );
   // }
 
   async getAllUser(): Promise<User[]> {
